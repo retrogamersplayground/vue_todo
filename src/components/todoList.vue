03946b37d873bf602bfba69178ef3795f8b12889
Neil Floyd <neilfloyd99@gmail.com>
2019-10-18 23:06:21 -0400

    first commit

---
+++ src/components/todoList.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="todoList">
+    <h1>Todo List</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'todoList',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  h1 {
+    text-align: center;
+  }
+</style>
